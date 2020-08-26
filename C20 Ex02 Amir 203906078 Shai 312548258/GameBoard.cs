99203b6f3c9c6ecca12f4b344d21cccc29327ab9
amirbs1 <amirbs1@gmail.com>
2020-08-26 17:57:56 +0300

    gameboard class add + user inputs

---
+++ C20 Ex02 Amir 203906078 Shai 312548258/GameBoard.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C20_Ex02
+{
+    class GameBoard
+    {
+        private int m_BoardSize = 0;
+        public const string k_SoldierO = " O ";
+        public const string k_SoldierX = " X ";
+
+        public GameBoard(int i_BoardSize)
+        {
+            this.m_BoardSize = i_BoardSize;
+        }
+
+        public int BoardSize
+        {
+            get
+            {
+                return m_BoardSize;
+            }
+
+            set
+            {
+                m_BoardSize = value;
+            }
+        }
+
+        /// <summary>
+        /// This method prints the game board
+        /// </summary>
+        /// <param name="i_BoardSize">the board size represented by one digit</param>
+        public static void PrintBoard(int i_BoardSize)
+        {
+            string[,] arr = new string[i_BoardSize,i_BoardSize];
+
+            int rowLength = arr.GetLength(0);
+            int colLength = arr.GetLength(1);
+            string soldierType = k_SoldierO;
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                if (i == (rowLength / 2) + 1)
+                {
+                    soldierType = k_SoldierX;
+                }
+                else if(i == (rowLength / 2) - 1 || i == (rowLength / 2))
+                {
+                    soldierType = "   ";
+                }
+
+                printRowsSeparator(rowLength);
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("|"));
+                    if ((i % 2) == 0)
+                    {
+                        if ((j % 2) != 0)
+                        {
+                            Console.Write(string.Format("{0}", soldierType));
+                        }
+                        else
+                        {
+                            Console.Write(string.Format("   "));
+                        }
+                    }
+                    else
+                    {
+                        if ((j % 2) == 0)
+                        {
+                            Console.Write(string.Format("{0}", soldierType));
+                        }
+                        else
+                        {
+                            Console.Write(string.Format("   "));
+                        }
+                    }
+                }
+
+                Console.Write(string.Format("|"));
+                Console.Write(Environment.NewLine);
+            }
+
+            printRowsSeparator(rowLength);
+        }
+
+        /// <summary>
+        /// This method prints the rows separators.
+        /// </summary>
+        /// <param name="i_numberOfRows"> number of rows</param>
+        private static void printRowsSeparator(int i_numberOfRows)
+        {
+            if (i_numberOfRows == 6)
+            {
+                Console.WriteLine("=========================");
+            }
+            else if (i_numberOfRows == 8)
+            {
+                Console.WriteLine("=================================");
+            }
+            else if (i_numberOfRows == 10)
+            {
+                Console.WriteLine("=========================================");
+            }
+
+
+
+        }
+    }
+}
--- C20 Ex02 Amir 203906078 Shai 312548258/Program.cs
+++ C20 Ex02 Amir 203906078 Shai 312548258/Program.cs
@@ -9,50 +9,72 @@ namespace C20_Ex02
 {
     class Program
     {
+
+        public static string GetUserName()
+        {
+            Console.WriteLine("Welcome to The Checkers Game!");
+            string userName = string.Empty;
+            bool checkUserName = true;
+
+            while (checkUserName)
+            {
+                Console.WriteLine("Please Enter your Name (without spaces): ");
+                userName = Console.ReadLine();
+
+                if (userName.Length > 20)
+                {
+                    Console.WriteLine("The name provided is longer than 20 characters. Try again");
+                    continue;
+                }
+                else if (userName.Contains(" "))
+                {
+                    Console.WriteLine("Please provide a string without spaces. Try again.");
+                    continue;
+                }
+                else
+                {
+                    checkUserName = false;
+                }
+            }
+
+            return userName;
+        }
+
+        public static int GetBoardSizeFromUser()
+        {
+            string boardSizeFromUser = string.Empty;
+            int integerBoardSize = 0;
+            Console.WriteLine("Choose your game board size.");
+            Console.WriteLine("You can type 6 for (6x6), 8 for (8x8) or 10 for (10x10): ");
+            bool boardSizeInputCheck = true;
+
+            while (boardSizeInputCheck)
+            {
+                boardSizeFromUser = Console.ReadLine();
+                bool isBoardSizeInteger = int.TryParse(boardSizeFromUser, out integerBoardSize);
+                if (integerBoardSize != 6 && integerBoardSize != 8 && integerBoardSize != 10)
+                {
+                    Console.WriteLine("Wrong size. Please choose 6, 8 or 10.");
+                    continue;
+                }
+
+                boardSizeInputCheck = false;
+            }
+
+            return integerBoardSize;
+        }
+
         public static void Main()
         {
-            string[,] arr = new string[8, 8];
+           
+            string name = GetUserName();
+            int boardSize = GetBoardSizeFromUser();
+            GameBoard b = new GameBoard(boardSize);
 
-            int rowLength = arr.GetLength(0);
-            int colLength = arr.GetLength(1);
-            string Oletter = " O ";
+            GameBoard.PrintBoard(boardSize);
+            Console.WriteLine(boardSize);
 
-            for (int i = 0; i < (rowLength/2) -1; i++)
-            {
-                Console.WriteLine("=================================");
-                for (int j = 0; j < colLength; j++)
-                {
-                    Console.Write(string.Format("|"));
-                    if ((i % 2) == 0)
-                    {
-                        if ((j % 2) != 0)
-                        {
-                            Console.Write(string.Format("{0}", Oletter));
 
-                        }
-                        else
-                        {
-                            Console.Write(string.Format("   "));
-                        }
-                    }
-                    else
-                    {
-                        if ((j % 2) == 0)
-                        {
-                            Console.Write(string.Format("{0}", Oletter));
-                        }
-                        else
-                        {
-                            Console.Write(string.Format("   "));
-                        }
-                    }
-                }
-                Console.Write(string.Format("|"));
-                Console.Write(Environment.NewLine); 
-            }
-            Console.WriteLine("=================================");
-
-            Console.ReadLine();
         }
     }
 }
