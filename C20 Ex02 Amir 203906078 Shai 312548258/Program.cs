ced8b629bc7e754e227a8cf2fab50635f56f9462
shai <shaioren22@gmail.com>
2020-08-26 10:15:20 +0300

    c20 ex02 starting proj

---
+++ C20 Ex02 Amir 203906078 Shai 312548258/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C20_Ex02_Amir_203906078_Shai_312548258
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
