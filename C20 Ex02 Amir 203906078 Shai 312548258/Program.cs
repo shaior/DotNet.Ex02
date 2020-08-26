57d09910aa7c516c47e2b19e692ce5e9c27ce798
shai <shaioren22@gmail.com>
2020-08-26 14:02:55 +0300

    Change the matrix to fit the look

--- C20 Ex02 Amir 203906078 Shai 312548258/Program.cs
+++ C20 Ex02 Amir 203906078 Shai 312548258/Program.cs
@@ -4,13 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace C20_Ex02
 {
     class Program
     {
         public static void Main()
         {
-            //test
+            string[,] arr = new string[8, 8];
+
+            int rowLength = arr.GetLength(0);
+            int colLength = arr.GetLength(1);
+            string letter = "O";
+
+            for (int i = 0; i < (rowLength/2) -1; i++)
+            {
+                Console.WriteLine("=======================================");
+                for (int j = 0; j < colLength; j++)
+                {
+                    if ((i % 2) == 0)
+                    {
+                        if ((j % 2) != 0)
+                        {
+                            Console.Write(string.Format("|"));
+                            Console.Write(string.Format(" {0}  ", letter));
+                            Console.Write(string.Format("|"));
+                        }
+                        else
+                        {
+                            Console.Write(string.Format("   "));
+                        }
+                    }
+                    else
+                    {
+                          if ((j % 2) == 0)
+                        {
+                            Console.Write(string.Format(" {0}  ", letter));
+                        }
+                        else
+                        {
+                            Console.Write(string.Format("|   | "));
+                        }
+                    }
+                  
+                }
+
+                Console.Write(Environment.NewLine); 
+            }
+            Console.WriteLine("=======================================");
+
+            Console.ReadLine();
         }
     }
 }
