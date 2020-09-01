7fea9b839d3f770e15f7af3444083ed73c0640d4
shai <shaioren22@gmail.com>
2020-09-01 15:32:24 +0300

    push

---
+++ C20 Ex02 Amir 203906078 Shai 312548258/Pawn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C20_Ex02
+{
+    public class Pawn
+    {
+        private string m_PlayerPawnType;
+
+        public Pawn(string i_PlayerPawn)
+        {
+
+            this.m_PlayerPawnType = i_PlayerPawn;
+        }
+        public string PawnType
+        {
+            get
+            {
+                return m_PlayerPawnType;
+            }
+        }
+
+        public enum ePawns
+        {
+            KingX = 'K',
+            KingO = 'U',
+            X = 'X',
+            O = 'O',
+
+
+        }
+
+    }
+}
